17731a93b4aa143cf9f715ec15871f6736263fb9
BubbleShade <ozypfl@gmail.com>
2024-07-21 02:08:09 -0700

    Made sun slider, added shadow hitboxes, added lamp-post

--- Scripts/Shadow.gd
+++ Scripts/Shadow.gd
@@ -4,28 +4,38 @@ extends Sprite2D
 # 0.5 is behind the object
 # 1 is to the right of the object
 
-@export var shadowScale = 5
+@export var shadowScale = Vector2(5,1.5)
+@export var shadowScaleY = 1.5
+#@onready var sprite = get_node("Sprite")
+@onready var baseOrigin = -offset
+#var shader_value = material.get_shader_param("level")
 # Called when the node enters the scene tree for the first time.
-func _ready(): 
-	transform.origin = -offset + Vector2(0, -8)
+func _ready(): pass
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# https://docs.godotengine.org/en/stable/tutorials/math/matrices_and_transforms.html
 	
+	#print(shader_value)
+	# https://docs.godotengine.org/en/stable/tutorials/math/matrices_and_transforms.html
 	var newTransform = Transform2D() 	# Creates a new transform object
 	#Modified shadow progess, this looks weird, but it just clamps ShadowProgress between 0 and 1,
 	#And also makes it so it bouces, so 1.1 becomes 0.9, instead of 0.1
 	var modSP = abs(fmod(abs(LevelInfo.ShadowProgress), 2.0) -1)
-	
+
+
+
 	#The sin of the ModShadowProgress * PI. This is done to create the effect so when 
 	# ShadowProgress is 0.5, it is behind the object (sin(pi/2) = 1) and when it is 0 or 1 it is at one of the extremes
 	# (sin(pi) = sin(0) = 0)
 	var sinSP = sin(modSP*PI) 
 	
+	var cscSP = 1/sin(modSP*PI) 
+	
 	#The sin of the ModShadowProgress * PI. This is done to create the effect so when 
 	# ShadowProgress is 0.5, it is behind the object (sin(pi/2) = 1) and when it is 0 or 1 it is at one of the extremes
 	# (sin(pi) = sin(0) = 0)
-	var cosSP = -cos(modSP*PI) 
+	var cosSP = cos(modSP*PI) 
 
 	# (modSP*shadowScale*2)-shadowScale - this is the x component of the y compenent
 	# when modSP = 0, the x compenent is -shadowScale
@@ -33,18 +43,17 @@ func _process(delta):
 	# when modSP = 1, the x compenent is shadowScale
 	# TODO: Try switching this with cos
 	# The y compenent of the y is set to SP to emulate a circular motion in the shaows transformation
-	newTransform.y = Vector2((modSP*shadowScale*2)-shadowScale,sinSP)
+	newTransform.y = Vector2(-cosSP,sinSP) * shadowScale
 	
 	# if modSP > 0.5, flip the x component of the the t.x of the transform in order flip the sprite
 	# (Otherwise it looks weird IDK)
-	# TODO: Try switching with cos
 	# The y component of the x is set to 1 - sinSP, in order to keep the t.y.y + t.x.y = 1, otherwise it'll become flat
-
-	if(modSP > 0.5): newTransform.x = Vector2(-sinSP,1-sinSP)
-	else: newTransform.x = Vector2(sinSP,1-sinSP)
-	
+	newTransform.x = Vector2(sinSP,cosSP)
+	#if(modSP > 0.5): newTransform.x = Vector2(-sinSP,cosSP)
+	#else: newTransform.x = Vector2(sinSP,cosSP)
+	var bounceSP = abs((modSP*2)-1)
 	# Change to move the sprite around idk
-	newTransform.origin = transform.origin
-	
+	newTransform.origin = baseOrigin #- Vector2(-cosSP*8, (bounceSP)*8)
+
 	# Update the sprites transform
 	transform = newTransform
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,26 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 150
+@export var accel = 800
+@onready var shadowbox: Area2D = get_node("ShadowHitbox")
+func Vector2Clamp(inputVector, v1: Vector2, v2: Vector2):
+	return Vector2(max(v1.x, min(inputVector.x, v2.x)),
+					max(v1.y, min(inputVector.y, v2.y)))
+	
 
 func _physics_process(delta):
 	var input_direction = Input.get_vector("p_left", "p_right", "p_up", "p_down")
-	velocity = input_direction * speed
+	velocity = Vector2Clamp(velocity + (input_direction * accel * delta),
+		Vector2(-speed,-speed), Vector2(speed, speed))
+	if(input_direction.x == 0): velocity.x = 0
+	if(input_direction.y == 0): velocity.y = 0
 
 	move_and_slide()
+	
+	print(shadowbox.get_overlapping_bodies())
+	#for i in shadowbox.get_overlapping_bodies():
+	#	print("I collided with ")
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		print("I collided with ", collision.collider.name)
+		
