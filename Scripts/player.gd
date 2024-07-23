ac0792ebf34d265b82ac28a345eccdc2d5e831ed
BubbleShade <ozypfl@gmail.com>
2024-07-22 20:23:47 -0700

    Player can only move when touching shadow or grass

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,26 +1,47 @@
-extends CharacterBody2D
+extends Node
 
 @export var speed = 150
 @export var accel = 800
-@onready var shadowbox: Area2D = get_node("ShadowHitbox")
+@onready var charBody: CharacterBody2D = get_node("Body2D")
+@onready var shadowbox: Area2D = charBody.get_node("ShadowHitbox")
+
+var collidingSafeBodies = []
+@onready var lastPosition = charBody.position
 func Vector2Clamp(inputVector, v1: Vector2, v2: Vector2):
 	return Vector2(max(v1.x, min(inputVector.x, v2.x)),
 					max(v1.y, min(inputVector.y, v2.y)))
+func getVelocity(delta):
+	var input_direction = Input.get_vector("p_left", "p_right", "p_up", "p_down")
+	var newVelocity = charBody.velocity + (input_direction * accel * delta)
+	newVelocity = Vector2Clamp(newVelocity, Vector2(-speed,-speed), Vector2(speed, speed))
+	if(input_direction.x == 0): newVelocity.x = 0
+	if(input_direction.y == 0): newVelocity.y = 0
+	return newVelocity
+func isSafe():
+	var tilemap : TileMap = get_node("../TileMap")
+	var tileBelow := tilemap.local_to_map(charBody.global_position) 
+	var tileExists = tilemap.get_cell_source_id(1, tileBelow) != -1
+	
+	return (len(collidingSafeBodies) != 0) || tileExists
 	
 
 func _physics_process(delta):
-	var input_direction = Input.get_vector("p_left", "p_right", "p_up", "p_down")
-	velocity = Vector2Clamp(velocity + (input_direction * accel * delta),
-		Vector2(-speed,-speed), Vector2(speed, speed))
-	if(input_direction.x == 0): velocity.x = 0
-	if(input_direction.y == 0): velocity.y = 0
-
-	move_and_slide()
 	
-	print(shadowbox.get_overlapping_bodies())
-	#for i in shadowbox.get_overlapping_bodies():
-	#	print("I collided with ")
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		print("I collided with ", collision.collider.name)
-		
+	lastPosition = charBody.position
+	#print(lastPosition, "Before")
+	charBody.velocity = getVelocity(delta)
+	charBody.move_and_slide()
+	#print(charBody.velocity)
+	#else:
+	#	charBody.position = sprite.position
+func _process(delta):
+	if(!isSafe()): charBody.position = lastPosition
+	
+func _ready():
+	shadowbox.area_entered.connect(onBodyEnter)
+	shadowbox.area_exited.connect(onBodyExit)
+	
+func onBodyEnter(body: Node):
+	collidingSafeBodies.append(body)
+func onBodyExit(body: Node):
+	collidingSafeBodies.remove_at(collidingSafeBodies.find(body))
